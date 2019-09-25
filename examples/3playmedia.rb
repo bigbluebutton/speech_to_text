77553c2de5957f11e45e582df34b3b3dddd38786
parthikmodi <parthik.modi10@gmail.com>
2019-09-25 08:24:08 -0700

    fixed couple of rubocop offence

--- examples/3playmedia.rb
+++ examples/3playmedia.rb
@@ -1,94 +1,97 @@
+# frozen_string_literal: true
+
 require 'json'
 
-#method will create job and retun job_id
-#Note that it will not generate captions automatically
-#you have to tell 3playmedia to generate captions depending on turnaround_level_id(could be anything between 1 to 6 depending on urgency level)
-#turnaround_level_id = 6 mean highest priority and minimum amout of time
-def create_job(api_key,audio_file,name,create_job_file)
+# method will create job and retun job_id
+# Note that it will not generate captions automatically
+# you have to tell 3playmedia to generate captions depending on turnaround_level_id(could be anything between 1 to 6 depending on urgency level)
+# turnaround_level_id = 6 mean highest priority and minimum amout of time
+def create_job(api_key, audio_file, name, create_job_file)
   cretae_job_command = "curl -X POST -F \"source_file=@#{audio_file}\" \"https://api.3playmedia.com/v3/files?api_key=#{api_key}&language_id=1&name=#{name}\" > #{create_job_file}"
   system(cretae_job_command)
-  file = File.open(create_job_file, "r")
+  file = File.open(create_job_file, 'r')
   response = JSON.load file
-  job_id = response["data"]["id"]
-  return job_id
+  job_id = response['data']['id']
+  job_id
 end
 
-#get transcription id so you can check status of transcription
-def order_transcript(api_key,job_id,turnaround_level_id,order_transcript_file)
+# get transcription id so you can check status of transcription
+def order_transcript(api_key, job_id, turnaround_level_id, order_transcript_file)
   order_transcript_commad = "curl -X POST \"https://api.3playmedia.com/v3/transcripts/order/transcription?api_key=#{api_key}&media_file_id=#{job_id}&turnaround_level_id=#{turnaround_level_id}\" >#{order_transcript_file}"
   system(order_transcript_commad)
-  file = File.open(order_transcript_file, "r")
+  file = File.open(order_transcript_file, 'r')
   response = JSON.load file
-  transcript_id = response["data"]["id"]
-  return transcript_id
+  transcript_id = response['data']['id']
+  transcript_id
 end
 
-def check_status(api_key,transcript_id,status_file)
+def check_status(api_key, transcript_id, status_file)
   check_status_command = "curl \"https://api.3playmedia.com/v3/transcripts/#{transcript_id}?api_key=#{api_key}\">#{status_file}"
   system(check_status_command)
-  file = File.open(status_file, "r")
+  file = File.open(status_file, 'r')
   response = JSON.load file
-  status = response["data"]["status"]
-  return status
+  status = response['data']['status']
+  status
 end
 
-def download_transcript(api_key,output_format_id,transcript_id,trascript_json_file)
-download_transcript_command = "curl \"https://api.3playmedia.com/v3/transcripts/#{transcript_id}/text?api_key=#{api_key}&output_format_id=#{output_format_id}\" > #{trascript_json_file}"
-system(download_transcript_command)
+def download_transcript(api_key, output_format_id, transcript_id, trascript_json_file)
+  download_transcript_command = "curl \"https://api.3playmedia.com/v3/transcripts/#{transcript_id}/text?api_key=#{api_key}&output_format_id=#{output_format_id}\" > #{trascript_json_file}"
+  system(download_transcript_command)
 end
 
-def create_vttfile(trascript_json_file,vtt_file)
-  file = File.open(trascript_json_file,"r")
+def create_vttfile(trascript_json_file, vtt_file)
+  file = File.open(trascript_json_file, 'r')
   response = JSON.load file
-  data = response["data"]
+  data = response['data']
   file.close
-  out = File.open(vtt_file,"w")
+  out = File.open(vtt_file, 'w')
   out.puts data
   out.close
 end
 
-def deleteFiles(create_job_file,order_transcript_file,status_file,trascript_json_file)
+# rubocop:disable Naming/MethodName
+def deleteFiles(create_job_file, order_transcript_file, status_file, trascript_json_file)
   File.delete(create_job_file)
   File.delete(order_transcript_file)
   File.delete(status_file)
   File.delete(trascript_json_file)
 end
+# rubocop:enable Naming/MethodName
 #---------------------
-#main code starts here
+# main code starts here
 #---------------------
 
 api_key = ARGV[0]
-audio_file = ARGV[1] #full audio path /home/a/b/audio.flac
-name = ARGV[2] #anything to identify job (can be duplicate doesn't really matter)
-turnaround_level_id = ARGV[3] #could be anything between 1 to 6 (1 means lowest priority)
+audio_file = ARGV[1] # full audio path /home/a/b/audio.flac
+name = ARGV[2] # anything to identify job (can be duplicate doesn't really matter)
+turnaround_level_id = ARGV[3] # could be anything between 1 to 6 (1 means lowest priority)
 output_format_id = ARGV[4] # output format could be anything, i.e plain text, srt file, vttfile etc..  use 139 for vtt_file and 7 for srt file
 
-#json file list
-create_job_file = ARGV[5] #/home/a/b/xyz.json
-order_transcript_file = ARGV[6] #/home/a/b/abc.json
-status_file = ARGV[7] #/home/a/b/mn.json
-trascript_json_file = ARGV[8] #/home/a/b/xxx.json
+# json file list
+create_job_file = ARGV[5] # /home/a/b/xyz.json
+order_transcript_file = ARGV[6] # /home/a/b/abc.json
+status_file = ARGV[7] # /home/a/b/mn.json
+trascript_json_file = ARGV[8] # /home/a/b/xxx.json
 
-#vtt file list
-vtt_file = ARGV[9] #/home/a/b/vttfile.vtt
+# vtt file list
+vtt_file = ARGV[9] # /home/a/b/vttfile.vtt
 
-job_id = create_job(api_key,audio_file,name,create_job_file)
-transcript_id = order_transcript(api_key,job_id,turnaround_level_id,order_transcript_file)
-status = check_status(api_key,transcript_id,status_file)
-second = 0 #remove me
-while (status!="complete")
-  status = check_status(api_key,transcript_id,status_file)
+job_id = create_job(api_key, audio_file, name, create_job_file)
+transcript_id = order_transcript(api_key, job_id, turnaround_level_id, order_transcript_file)
+status = check_status(api_key, transcript_id, status_file)
+second = 0 # remove me
+while status != 'complete'
+  status = check_status(api_key, transcript_id, status_file)
   puts status
   puts second
-  if(status == "cancelled")
-    break
-  end
+  break if status == 'cancelled'
+
   sleep(30)
-  second += 31  #remove me, it is just to check time
+  second += 31 # remove me, it is just to check time
 end
 
-if(status == "complete")
-  download_transcript(api_key,output_format_id,transcript_id,trascript_json_file)
-  create_vttfile(trascript_json_file,vtt_file)
-  deleteFiles(create_job_file,order_transcript_file,status_file,trascript_json_file)
+if status == 'complete'
+  download_transcript(api_key, output_format_id, transcript_id, trascript_json_file)
+  create_vttfile(trascript_json_file, vtt_file)
+  deleteFiles(create_job_file, order_transcript_file, status_file, trascript_json_file)
 end
--- lib/speech_to_text/3playmedia.rb
+++ lib/speech_to_text/3playmedia.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Set encoding to utf-8
 # encoding: UTF-8
 
@@ -11,49 +13,52 @@ require 'net/http'
 require 'uri'
 
 module SpeechToText
-	module ThreePlaymediaS2T
-    def self.create_job(api_key,audio_file,name,create_job_file)
+  module ThreePlaymediaS2T # rubocop:disable Style/Documentation
+    def self.create_job(api_key, audio_file, name, create_job_file)
       cretae_job_command = "curl -X POST -F \"source_file=@#{audio_file}\" \"https://api.3playmedia.com/v3/files?api_key=#{api_key}&language_id=1&name=#{name}\" > #{create_job_file}"
       system(cretae_job_command)
-      file = File.open(create_job_file, "r")
+      file = File.open(create_job_file, 'r')
       response = JSON.load file
-      job_id = response["data"]["id"]
-      return job_id
+      job_id = response['data']['id']
+      job_id
     end
 
-    def self.order_transcript(api_key,job_id,turnaround_level_id)
+    # rubocop:disable Metrics/MethodLength
+    def self.order_transcript(api_key, job_id, turnaround_level_id)
       uri = URI.parse("https://api.3playmedia.com/v3/transcripts/order/transcription?api_key=#{api_key}&media_file_id=#{job_id}&turnaround_level_id=#{turnaround_level_id}")
       request = Net::HTTP::Post.new(uri)
       req_options = {
-        use_ssl: uri.scheme == "https",
+        use_ssl: uri.scheme == 'https'
       }
 
       response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
         http.request(request)
       end
       res = JSON.load response.body
-      transcript_id = res["data"]["id"]
-      return transcript_id
+      transcript_id = res['data']['id']
+      transcript_id
     end
+    # rubocop:enable Metrics/MethodLength
 
-    def self.check_status(api_key,transcript_id)
+    def self.check_status(api_key, transcript_id)
       uri = URI.parse("https://api.3playmedia.com/v3/transcripts/#{transcript_id}?api_key=#{api_key}")
       response = Net::HTTP.get_response(uri)
       res = JSON.load response.body
-      status = res["data"]["status"]
+      status = res['data']['status']
+      status
     end
 
-    def self.get_vttfile(api_key,output_format_id,transcript_id,vtt_file_path,vtt_file_name)
+    def self.get_vttfile(api_key, output_format_id, transcript_id, vtt_file_path, vtt_file_name)
       uri = URI.parse("https://api.3playmedia.com/v3/transcripts/#{transcript_id}/text?api_key=#{api_key}&output_format_id=#{output_format_id}")
       response = Net::HTTP.get_response(uri)
       res = JSON.load response.body
-      out = File.open("#{vtt_file_path}/#{vtt_file_name}","w")
-      out.puts res["data"]
+      out = File.open("#{vtt_file_path}/#{vtt_file_name}", 'w')
+      out.puts res['data']
       out.close
 
-			captions_file_name = "#{vtt_file_path}/captions.json"
-		  captions_file = File.open(captions_file_name,"w")
-		  captions_file.puts "[{\"localeName\": \"English (United States)\", \"locale\": \"en-US\"}]"
+      captions_file_name = "#{vtt_file_path}/captions.json"
+      captions_file = File.open(captions_file_name, 'w')
+      captions_file.puts '[{"localeName": "English (United States)", "locale": "en-US"}]'
       captions_file.close
     end
   end
